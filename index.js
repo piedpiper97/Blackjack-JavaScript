5046519d45af83afe694cad19831933bab98b847
piedpiper97 <chgtad006@myuct.ac.za>
2026-06-24 03:27:18 +0200

    new card functionality and random number generation

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
 // 2 cards with their values and their sum
-let one = 10;
-let two = 11;
-let sum = one + two;
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 
 let message = ""
 
-//black-jack logic
-if (sum < 21) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Whohoo! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You lose :("
-    isAlive = false
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+
+function startGame(){
+    isAlive = true
+    //generate initial cards
+    let one = randomize();
+    let two = randomize();
+    cards = [one, two]
+    sum = one + two
+    renderGame()
 }
-console.log(message)
+
+function renderGame(){
+    //initial state
+    cardsEl.textContent = "Cards: "
+    //get sum of current cards
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    //calculate sum
+    sumEl.textContent = "Sum: " + sum
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Whohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You lose :("
+        isAlive = false
+    }
+    messageEl.textContent = message;
+    
+}
+
+//user draws new card
+function newCard(){
+    let card = randomize();
+    sum += card;
+    cards.push(card)
+    renderGame();
+}
+
+//random number generator between 0 & 11
+function randomize(){
+    let num = Math.floor(Math.random() * 13) + 1
+    if (num > 10){
+        return 10
+    } else if (num === 1) {
+        return 11
+    } return num
+}
